6c441e7d6d690871b611109953c0d39f44b74a6c
Sergey Pavlov <self@sergeypavlov.dev>
2026-03-29 01:19:08 +0300

    update hunter

--- ms/bars/class/hunter.lua
+++ ms/bars/class/hunter.lua
@@ -8,20 +8,14 @@ MS.ActionBarSetters[MS.ClassID.HUNTER] = function()
     MS.PutSpellInSlot("Dismiss Pet", 11)
     MS.PutSpellInSlot("Beast Lore", 10)
 
+    MS.PutMacroInSlot("Hunter_b1", 1)
+    MS.PutMacroInSlot("Hunter_b2", 2)
+    MS.PutMacroInSlot("Hunter_b3", 3)
     MS.PutSpellInSlot("Revive Pet", 4)
 
     if MS.MySpecName == "Survival" then
-        MS.PutMacroInSlot("Kill Command", 1)
-        MS.PutMacroInSlot("Raptor Strike", 2)
-        MS.PutMacroInSlot("Wildfire Bomb", 3)
         MS.PutSpellInSlot("Eyes of the Beast", 9)
     elseif MS.MySpecName == "Marksmanship" then
-        MS.PutMacroInSlot("Steady Shot", 1)
-        MS.PutMacroInSlot("Arcane Shot", 2)
-        MS.PutMacroInSlot("Aimed Shot", 3)
         MS.PutSpellInSlot("Eagle Eye", 9)
-    else
-        MS.PutMacroInSlot("Steady Shot", 1)
-        MS.PutMacroInSlot("Arcane Shot", 2)
     end
 end
