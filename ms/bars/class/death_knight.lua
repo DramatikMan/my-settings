33bdd3f2fa1a70a1d8dd928a30f03141f5508e87
Sergey Pavlov <self@sergeypavlov.dev>
2026-03-31 20:06:46 +0300

    update death knight

--- ms/bars/class/death_knight.lua
+++ ms/bars/class/death_knight.lua
@@ -2,11 +2,12 @@ MS.ActionBarSetters[MS.ClassID.DEATHKNIGHT] = function()
     MS.PutActiveRacialInSlot(146)
     MS.PutSpellInSlot("Dark Command", 39, 147)
     MS.PutSpellInSlot("Raise Dead", 38)
-    
+
     MS.PutMacroInSlot("DK_b1", 1)
     MS.PutMacroInSlot("DK_b2", 2)
     MS.PutMacroInSlot("DK_b3", 3)
     MS.PutMacroInSlot("DK_b4", 4)
+    MS.PutMacroInSlot("DK_b5", 5)
 
     MS.PutSpellInSlot("Death Gate", 12)
 
@@ -14,13 +15,5 @@ MS.ActionBarSetters[MS.ClassID.DEATHKNIGHT] = function()
         if name == "Runeforging" then
             MS.PutSpellbookItemInSlot(spellBookItemSlotIndex, 11)
         end
-    end
-
-    if MS.MySpecName == "Frost" then
-        MS.PutSpellInSlot("Frost Strike", 1)
-        MS.PutSpellInSlot("Obliterate", 2)
-        MS.PutSpellInSlot("Frostscythe", 3)
-    else
-        MS.PutSpellInSlot("Rune Strike", 1)
     end
 end
